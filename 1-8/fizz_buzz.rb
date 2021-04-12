4b28dc266b73b768e3f483cda8a74f730e3edc6e
yuki-zirushi <yuko.12.151@gmail.com>
2021-04-12 23:57:14 +0000

    [Update]

--- 1-8/fizz_buzz.rb
+++ 1-8/fizz_buzz.rb
@@ -1,21 +1,21 @@
 def fizz_buzz(number)
-  
-  puts "数字を入力してください。"
-  
-  number = gets.to_i
-  
-  puts "結果は..."
-  
-  if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
+
+  if number % 15 == 0
+    "FizzBuzz"
   elsif number % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   else
-    puts "#{number.to_s}"
+    number.to_s
   end
-  
+
 end
 
-puts fizz_buzz("number")
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts "結果は"
+
+puts fizz_buzz(input)
